1e4aaec1c200eef99e46b86f6123ffc84063aa91
QuentinCAA <quentin.roques@climateactionaccelerator.org>
2025-01-28 16:33:30 +0100

    Initial commit - ajout de la maquette Streamlit

---
+++ app.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+import streamlit as st
+
+st.title("Hello, Streamlit!")
+st.write("This is a simple Streamlit app.")
